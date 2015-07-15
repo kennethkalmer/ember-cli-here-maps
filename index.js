f883eb35ec6f8934d07f608a47339563eb1d3376
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-07-15 23:37:16 +0200

    feat: inject mapjs into the document head

--- index.js
+++ index.js
@@ -10,5 +10,16 @@ module.exports = {
     app.import('vendor/here-shim.js', {
       exports: { 'H': ['default'] }
     });
+  },
+
+  contentFor: function(type, config) {
+    var content = "";
+
+    if (type === "head") {
+      content = "<script src=\"//js.api.here.com/v3/3.0/mapsjs-core.js\" type=\"text/javascript\" charset=\"utf-8\"></script>\n" +
+        "<script src=\"//js.api.here.com/v3/3.0/mapsjs-service.js\" type=\"text/javascript\" charset=\"utf-8\"></script>";
+    }
+
+    return content;
   }
 };
