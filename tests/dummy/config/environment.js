45a68ca8293a7e3ae9aa062674caae2f8748270e
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-07-15 23:51:39 +0200

    chore: dummy app configuration

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -16,6 +16,21 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-eval' *.api.here.com",
+      'font-src': "'self'",
+      'connect-src': "'self' *.api.here.com",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+    },
+
+    hereMaps: {
+      app_id: process.env.HERE_MAPS_APP_ID,
+      app_code: process.env.HERE_MAPS_APP_CODE
     }
   };
 
